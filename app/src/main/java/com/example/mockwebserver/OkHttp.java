2acd2ba31fe94155f4bee94543f195c7709cdd24
sourcecode121 <sourcecode121@users.noreply.github.com>
2017-01-18 18:02:55 +0530

    Adding certificate pinner

--- app/src/main/java/com/example/mockwebserver/OkHttp.java
+++ app/src/main/java/com/example/mockwebserver/OkHttp.java
@@ -1,5 +1,6 @@
 package com.example.mockwebserver;
 
+import okhttp3.CertificatePinner;
 import okhttp3.OkHttpClient;
 
 /**
@@ -11,8 +12,19 @@ public class OkHttp {
 
     public static OkHttpClient getInstance() {
         if (instance == null) {
-            instance = new OkHttpClient();
+            OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                    .certificatePinner(createCertificatePinner());
+            instance = builder.build();
         }
         return instance;
+    }
+
+    private static CertificatePinner createCertificatePinner() {
+        String host = "api.github.com";
+        return new CertificatePinner.Builder()
+                .add(host, "sha256/6wJsqVDF8K19zxfLxV5DGRneLyzso9adVdUN/exDacw=")
+                .add(host, "sha256/k2v657xBsOVe1PQRwOsHsw3bsGT2VzIqz5K+59sNQws=")
+                .add(host, "sha256/WoiWRyIOVNa9ihaBciRSC7XHjliYS9VwUGOIud4PB18=")
+                .build();
     }
 }
